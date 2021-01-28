1e528b48f7fe453d567159699c1dfedfeee6b33d
Owen Kaluza <owen.kaluza@monash.edu>
2021-01-28 14:37:22 +1100

    Debugging error message from mknod

--- scripts/nvidia-mkdevs.sh
+++ scripts/nvidia-mkdevs.sh
@@ -6,6 +6,7 @@ N3D=`echo "$NVDEVS" | grep "3D controller" | wc -l`
 NVGA=`echo "$NVDEVS" | grep "VGA compatible controller" | wc -l`
 N=`expr $N3D + $NVGA - 1`
 
+echo "NVDEVS="$NVDEVS $N3D $NVGA $N
 for i in `seq 0 $N`; do
     rm -f /dev/nvidia$i
     mknod -m 666 /dev/nvidia$i c 195 $i
@@ -16,5 +17,8 @@ mknod -m 666 /dev/nvidiactl c 195 255
 
 # Find out the major device number used by the nvidia-uvm driver
 D=`grep nvidia-uvm /proc/devices | awk '{print $1}'`
+echo "D=" $D
 rm -f /dev/nvidia-uvm
 mknod -m 666 /dev/nvidia-uvm c $D 0
+
+ls -ltr /dev/nv*
