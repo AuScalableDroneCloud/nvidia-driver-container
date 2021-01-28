bfd476e675681986e19fc7c59de23d9e03588cbf
Owen Kaluza <owen.kaluza@monash.edu>
2021-01-28 20:16:17 +1100

    Re-enable with debug echo

--- scripts/install-drv.sh
+++ scripts/install-drv.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-#depmod -a || true
-#modprobe -r nouveau || true
-#modprobe nvidia
-#modprobe nvidia-uvm
+echo 1
+depmod -a || true
+echo 2
+modprobe -r nouveau || true
+echo 3
+modprobe nvidia
+echo 4
+modprobe nvidia-uvm
+echo 5
 nvidia-mkdevs
+echo 6
 cp -rfn ${NVIDIA_PATH}/bin /opt/nvidia-host
 cp -rfn ${NVIDIA_PATH}/lib64 /opt/nvidia-host
